0f4acf7f756a29748eb29189af9bfe1d2b89ef77
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-04-17 03:10:04 +0300

    feat: add main js file

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
+function mergeUserArrays(...arrays) {
+  const mergedArray = [].concat(...arrays);
 
+  const mergeDate = new Date();
+  
+  return {
+    users: mergedArray,
+    mergeDate: mergeDate,
+    totalUsers: mergedArray.length
+  };
+}
+
+const usersFromSiteA = ["alice123", "bob_dev", "charlie_codes"];
+const usersFromSiteB = ["diana_smith", "eve_wilson", "frank_lee"];
+const usersFromSiteC = ["grace_lin"];
+
+const result = mergeUserArrays(
+  usersFromSiteA,
+  usersFromSiteB,
+  usersFromSiteC
+);
+
+console.log(result);
 
 
 module.exports = {
